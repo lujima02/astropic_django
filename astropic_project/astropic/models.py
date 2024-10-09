cae0409e772289309a98d9f619d25265d3ba963f
lujima02 <lujima2002@gmail.com>
2024-10-09 20:00:37 +0200

    3

--- astropic_project/astropic/models.py
+++ astropic_project/astropic/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='photos/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey('AstronomicalEvent', null=True, blank=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.photo.title}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'photo')
+    
+    def __str__(self):
+        return f'{self.user.username} likes {self.photo.title}'
+
+class AstronomicalEvent(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    event_date = models.DateField()
+    
+    def __str__(self):
+        return self.name
