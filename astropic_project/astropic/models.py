a533bc490826722494f5007551faf06d989448c5
lujima02 <lujima2002@gmail.com>
2024-10-09 21:05:26 +0200

    5

--- astropic_project/astropic/models.py
+++ astropic_project/astropic/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Photo(models.Model):
     title = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='photos/')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,6 +12,15 @@ class Photo(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def add_like(self):
+        self.likes += 1
+        self.save()
+
+    def remove_like(self):
+        if self.likes > 0:
+            self.likes -= 1
+        self.save()
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,7 +29,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return f'Comment by {self.user.username} on {self.photo.title}'
+        return f'{self.user.username} comentó en {self.photo.title}'
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
