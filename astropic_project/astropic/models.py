db16bf2a7823f3a494d6112c03268380e624843a
lujima02 <lujima2002@gmail.com>
2024-10-14 21:40:19 +0200

    html

--- astropic_project/astropic/models.py
+++ astropic_project/astropic/models.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import User
 class Photo(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='photos/')
+    image = models.ImageField(upload_to='photos/', blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey('AstronomicalEvent', null=True, blank=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
+    
+    def like_count(self):
+        return self.like_set.count()
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- astropic_project/astropic_project/settings.py
+++ astropic_project/astropic_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'astropic_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'astropic/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILE_DIR = [os.path.join(BASE_DIR, 'astropic/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
