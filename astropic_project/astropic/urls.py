52ce0ea206d2c3baea7727ef4334a8abd1cfde9f
lujima02 <lujima2002@gmail.com>
2024-10-14 20:20:25 +0200

    views

--- astropic_project/astropic/urls.py
+++ astropic_project/astropic/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-
-from . import views
+from .views import (
+    PhotoListView, PhotoDetailView, AddCommentView, LikePhotoView, AstronomicalEventListView, AstronomicalEventDetailView
+)
 
 urlpatterns = [
-    
+    path('', PhotoListView.as_view(), name='photo-list'),
+    path('photo/<int:pk>/', PhotoDetailView.as_view(), name='photo-detail'),
+    path('photo/<int:pk>/comment/', AddCommentView.as_view(), name='add-comment'),
+    path('photo/<int:pk>/like/', LikePhotoView.as_view(), name='like-photo'),
+    path('events/', AstronomicalEventListView.as_view(), name='event-list'),
+    path('event/<int:pk>/', AstronomicalEventDetailView.as_view(), name='event-detail'),
 ]
--- astropic_project/astropic/views.py
+++ astropic_project/astropic/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Photo, Comment, Like, AstronomicalEvent
+from django.http import JsonResponse
 
+# Lista de fotos
+class PhotoListView(ListView):
+    model = Photo
+    template_name = 'photos/photo_list.html'  # Debes crear este template
+    context_object_name = 'photos'
+    paginate_by = 12  # Número de fotos por página
+
+# Detalle de la foto
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = 'photos/photo_detail.html'  # Debes crear este template
+    context_object_name = 'photo'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.filter(photo=self.object)
+        context['like_count'] = Like.objects.filter(photo=self.object).count()
+        return context
+
+# Crear un comentario
+class AddCommentView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        photo = get_object_or_404(Photo, pk=pk)
+        comment_text = request.POST.get('comment')
+        if comment_text:
+            Comment.objects.create(user=request.user, photo=photo, comment=comment_text)
+        return redirect('photo-detail', pk=pk)
+
+# Manejar likes
+class LikePhotoView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        photo = get_object_or_404(Photo, pk=pk)
+        like, created = Like.objects.get_or_create(user=request.user, photo=photo)
+        if not created:
+            # Si ya existe el like, lo eliminamos (dislike)
+            like.delete()
+        like_count = Like.objects.filter(photo=photo).count()
+        return JsonResponse({'like_count': like_count})
+
+# Lista de eventos astronómicos
+class AstronomicalEventListView(ListView):
+    model = AstronomicalEvent
+    template_name = 'events/event_list.html'  # Debes crear este template
+    context_object_name = 'events'
+    paginate_by = 10  # Número de eventos por página
+
+# Detalle de evento con fotos asociadas
+class AstronomicalEventDetailView(DetailView):
+    model = AstronomicalEvent
+    template_name = 'events/event_detail.html'  # Debes crear este template
+    context_object_name = 'event'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['photos'] = Photo.objects.filter(event=self.object)
+        return context
